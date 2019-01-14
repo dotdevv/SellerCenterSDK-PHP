2ef97145e77b20a8c9331737b3904f9a598b55d5
Monmon <monmon@dotdev.com.au>
2019-01-14 14:58:54 +1100

    :star2: Add FeedStatus action related functions

--- src/RocketLabs/SellerCenterSdk/Endpoint/Feed.php
+++ src/RocketLabs/SellerCenterSdk/Endpoint/Feed.php
@@ -4,6 +4,7 @@ namespace RocketLabs\SellerCenterSdk\Endpoint;
 
 use RocketLabs\SellerCenterSdk\Endpoint\Feed\Request\FeedList;
 use RocketLabs\SellerCenterSdk\Endpoint\Feed\Request\FeedOffsetList;
+use RocketLabs\SellerCenterSdk\Endpoint\Feed\Request\FeedStatus;
 
 /**
  * Class Feed
@@ -24,5 +25,15 @@ class Feed
     public function feedOffsetList($offset = 0, $pageSize = 10, $status = null, $creationDate = null, $updatedDate = null)
     {
         return new FeedOffsetList($offset, $pageSize, $status, $creationDate, $updatedDate);
+    }
+
+    /**
+     * @return FeedStatus
+     *
+     * @param $feedId
+     */
+    public function feedStatus($feedId)
+    {
+        return new FeedStatus($feedId);
     }
 }
---
+++ src/RocketLabs/SellerCenterSdk/Endpoint/Feed/Request/FeedStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace RocketLabs\SellerCenterSdk\Endpoint\Feed\Request;
+
+use RocketLabs\SellerCenterSdk\Core\Client;
+use RocketLabs\SellerCenterSdk\Core\Request\GenericRequest;
+use RocketLabs\SellerCenterSdk\Core\Response\ErrorResponse;
+use RocketLabs\SellerCenterSdk\Endpoint\Feed\Response\FeedStatus as FeedStatusResponse;
+
+/**
+ * Class FeedStatus
+ * @method FeedStatusResponse|ErrorResponse call(Client $client)
+ */
+class FeedStatus extends GenericRequest
+{
+    const ACTION = 'FeedStatus';
+
+    const FEED_ID = 'FeedID';
+
+    /**
+     * FeedStatus constructor.
+     *
+     * @param $feedId
+     */
+    public function __construct($feedId)
+    {
+        $query = [];
+
+        parent::__construct(
+            Client::GET,
+            static::ACTION,
+            static::V1,
+            [
+               static::FEED_ID => $feedId,
+            ]
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getResponseClassName()
+    {
+        return FeedStatusResponse::class;
+    }
+}
