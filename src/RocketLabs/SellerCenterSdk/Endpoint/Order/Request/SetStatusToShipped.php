0f06215940ecfdcc01bc00f3d917c2625c22f0dd
Monmon <monmon@dotdev.com.au>
2019-02-11 15:14:44 +1100

    :ship: Add SetStatusToShipped request

---
+++ src/RocketLabs/SellerCenterSdk/Endpoint/Order/Request/SetStatusToShipped.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RocketLabs\SellerCenterSdk\Endpoint\Order\Request;
+
+use RocketLabs\SellerCenterSdk\Core\Client;
+use RocketLabs\SellerCenterSdk\Core\Request\GenericRequest;
+
+/**
+ * Class SetStatusToShipped
+ */
+class SetStatusToShipped extends GenericRequest
+{
+    const ACTION = 'SetStatusToShipped';
+    const ORDER_ITEM_ID = 'OrderItemId';
+
+    /**
+     * SetStatusToShipped constructor.
+     * @param int $orderItemId
+     */
+    public function __construct($orderItemId)
+    {
+        parent::__construct(
+            Client::POST,
+            static::ACTION,
+            static::V1,
+            [
+                static::ORDER_ITEM_ID => $orderItemId
+            ]
+        );
+    }
+}
